d3370a9c3083dce4c09d4c64f8b46893c0feea32
heyichigo <he_yichigosann@outlook.com>
2024-04-26 16:42:42 +0800

    修改步骤计算

--- collection.py
+++ collection.py
@@ -89,8 +89,8 @@ def zip_file_list(file_list: list[tuple[str, datetime]], s_idx: int, e_idx: int)
             zip.write(file_path)
             # 添加删除操作
             os.remove(file_path)
-            i = (idx-s_idx)/(e_idx-s_idx)
-            logger.info(f"{zip_name[0:10]}: {i*100}% : {"▋"*(int(i)//2)}")
+            i = int((idx - s_idx) * 100 / (e_idx - s_idx))
+            logger.info(f"{zip_name[0:10]}: {i}% : {'▋'*(i/4)}")
     logger.info(f"already done {zip_name} at {zip_path}")
     return
 
